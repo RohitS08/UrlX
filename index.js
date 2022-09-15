1dc0c929145221fe05d80634bdb87460dabba43f
RohitS08 <rohitsinghmhkr@gmail.com>
2022-09-15 17:44:32 +0530

    #2 Solving attempt for * path errors!

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if(process.env.NODE_ENV=="production"){
   app.use(express.static("client_/build"));
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    res.sendFile(path.resolve(__dirname,'client_','build','index.html'))
   })
 }
 
